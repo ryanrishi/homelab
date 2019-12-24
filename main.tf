5fcde53ead1bc0d2e1cee85eb2413519ebb813a0
Ryan Rishi <ryan@ryanrishi.com>
2019-12-24 12:34:58 -0700

    fix policy JSON
    policy cannot have leading spaces:
    https://github.com/terraform-providers/terraform-provider-aws/issues/3970#issuecomment-389309609

--- main.tf
+++ main.tf
@@ -29,18 +29,18 @@ resource "aws_s3_bucket" "www" {
   bucket = var.www_domain_name
   acl    = "public-read"
   policy = <<POLICY
-  {
-    "Version": "2019-12-23",
-    "Statement": [
-      {
-        "Sid": "AddPerm",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": ["s3:GetObject"],
-        "Resource": ["arn:aws:s3:::${var.www_domain_name}/*"]
-      }
-    ]
-  }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AddPerm",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": ["arn:aws:s3:::${var.www_domain_name}/*"]
+    }
+  ]
+}
 POLICY
 
   website {
