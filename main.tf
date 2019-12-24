08f0feed766e965e8e0618a04b6832ffbe465b2a
Ryan Rishi <ryan@ryanrishi.com>
2019-12-24 12:16:22 -0700

    output AWS account/ARN/user id
    use `terraform output` to print these values

--- main.tf
+++ main.tf
@@ -3,6 +3,20 @@ provider "aws" {
   profile = "ryanrishi"
 }
 
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+
 variable "www_domain_name" {
   default = "www.ryanrishi.com"
 }
