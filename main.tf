3f0145c0fcd727c04b0da79a2db5ab0d41fbe45c
Ryan Rishi <ryan@ryanrishi.com>
2019-12-24 11:08:23 -0700

    start adding terraform for ryanrishi.com
    Following this tutorial:
    https://medium.com/runatlantis/hosting-our-static-site-over-ssl-with-s3-acm-cloudfront-and-terraform-513b799aec0f

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "aws-east-1"
+}
+
+variable "www_domain_name" {
+  default = "www.ryanrishi.com"
+}
+
+variable "root_domain_name" {
+  default = "ryanrishi.com"
+}
+
+resource "aws_s3_bucket" "www" {
+  bucket = "${var.www_domain_name}"
+  
+  acl = "public_read"
+  
+  policy = <<POLICY
+  {
+    "Version": "2019-12-23",
+    "Statement": [
+      {
+        "Sid": "AddPerm",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": ["s3:GetObject"],
+        "Resource": ["arn:aws:s3:::${var.www_domain_name}/*"]
+      }
+    ]
+  }
+  POLICY
+  
+  website {
+    index_document = "index.html"
+    
+    error_document = "404.html"
+  }
+}
