4fe76871ae7dc5512befb1ccc384be51153a66e8
Ryan Rishi <ryan@ryanrishi.com>
2019-12-24 13:16:36 -0700

    extract variables and outputs to their own files

--- main.tf
+++ main.tf
@@ -7,30 +7,8 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = var.region
   profile = "ryanrishi"
-}
-
-data "aws_caller_identity" "current" {}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "caller_arn" {
-  value = data.aws_caller_identity.current.arn
-}
-
-output "caller_user" {
-  value = data.aws_caller_identity.current.user_id
-}
-
-variable "www_domain_name" {
-  default = "www.ryanrishi.com"
-}
-
-variable "root_domain_name" {
-  default = "ryanrishi.com"
 }
 
 resource "aws_s3_bucket" "www" {
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "www_domain_name" {
+  default = "www.ryanrishi.com"
+}
+
+variable "root_domain_name" {
+  default = "ryanrishi.com"
+}
