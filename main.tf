9f8cdb6e6a2c95f6f9527030b8adb8929929810c
Ryan Rishi <ryan@ryanrishi.com>
2019-12-26 13:53:41 -0700

    add ACM + CloudFront

--- main.tf
+++ main.tf
@@ -34,3 +34,56 @@ POLICY
     error_document = "404.html"
   }
 }
+
+resource "aws_acm_certificate" "certificate" {
+  domain_name               = "*.${var.root_domain_name}"
+  validation_method         = "EMAIL"
+  subject_alternative_names = [var.www_domain_name]
+}
+
+resource "aws_cloudfront_distribution" "www_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.www.website_endpoint
+    origin_id   = var.www_domain_name
+
+    custom_origin_config {
+      http_port               = "80"
+      https_port              = "443"
+      origin_protocol_policy  = "http-only"
+      origin_ssl_protocols    = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy  = "redirect-to-https"
+    compress                = true
+    allowed_methods         = ["GET", "HEAD"]
+    cached_methods          = ["GET", "HEAD"]
+    target_origin_id        = var.www_domain_name
+    min_ttl                 = 0
+    default_ttl             = 86400
+    max_ttl                 = 31536000
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+}
