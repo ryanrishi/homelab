040817b3003ed1e0102b2c1a770a08f5eedf54f7
Ryan Rishi <ryan@ryanrishi.com>
2019-12-26 23:51:13 -0700

    create A record for www domain

--- main.tf
+++ main.tf
@@ -89,3 +89,19 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     ssl_support_method  = "sni-only"
   }
 }
+
+resource "aws_route53_zone" "zone" {
+  name = var.root_domain_name
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name    = var.www_domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.www_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
