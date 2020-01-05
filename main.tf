ba098c0ffd3d89e418eeeb24d3b7f74b86439873
Ryan Rishi <ryan@ryanrishi.com>
2020-01-05 14:51:49 -0800

    create CloudFront distribution for root domain
    At this point, the following redirects work:
    http://ryanrishi.com → https://ryanrishi.com/
    http://www.ryanrishi.com → https://www.ryanrishi.com/
    
    Having trouble getting http://www.ryanrishi.com → https://ryanrishi.com
    to work.

--- main.tf
+++ main.tf
@@ -103,6 +103,55 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     }
   }
 
+  aliases = [var.root_domain_name]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
+resource "aws_cloudfront_distribution" "ryanrishi-com" {
+  origin {
+    domain_name = aws_s3_bucket.ryanrishi-com.website_endpoint
+    origin_id   = var.root_domain_name
+
+    custom_origin_config {
+      http_port               = "80"
+      https_port              = "443"
+      origin_protocol_policy  = "http-only"
+      origin_ssl_protocols    = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy  = "redirect-to-https"
+    compress                = true
+    allowed_methods         = ["GET", "HEAD"]
+    cached_methods          = ["GET", "HEAD"]
+    target_origin_id        = var.root_domain_name
+    min_ttl                 = 0
+    default_ttl             = 86400
+    max_ttl                 = 31536000
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
   aliases = [var.www_domain_name]
 
   restrictions {
