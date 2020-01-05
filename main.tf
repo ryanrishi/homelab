c1fa56f375351fe757e562a698371e8dbc3d6081
Ryan Rishi <ryan@ryanrishi.com>
2020-01-05 14:29:53 -0800

    create S3 bucket for root domain, redirect www bucket to non-www bucket

--- main.tf
+++ main.tf
@@ -30,6 +30,29 @@ resource "aws_s3_bucket" "www" {
 POLICY
 
   website {
+    redirect_all_requests_to = var.root_domain_name
+  }
+}
+
+resource "aws_s3_bucket" "ryanrishi-com" {
+  bucket = var.root_domain_name
+  acl    = "public-read"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AddPerm",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": ["arn:aws:s3:::${var.root_domain_name}/*"]
+    }
+  ]
+}
+POLICY
+
+  website {
     index_document = "index.html"
     error_document = "404.html"
   }
