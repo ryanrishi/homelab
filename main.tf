c9547ed750f056529ff1d1cee25ec453fc61a40e
Ryan Rishi <ryan@ryanrishi.com>
2020-01-05 17:48:23 -0700

    Redirect www to root (#1)
    * create certificate for root domain
    
    * create A record for root domain
    
    * create S3 bucket for root domain, redirect www bucket to non-www bucket
    
    * create CloudFront distribution for root domain
    
    At this point, the following redirects work:
    http://ryanrishi.com → https://ryanrishi.com/
    http://www.ryanrishi.com → https://www.ryanrishi.com/
    
    Having trouble getting http://www.ryanrishi.com → https://ryanrishi.com
    to work.
    
    * rm default_root_object from CloudFront distributions
    
    * use HTTPS for redirects
    
    * use same CNAME as distribution

--- main.tf
+++ main.tf
@@ -30,15 +30,42 @@ resource "aws_s3_bucket" "www" {
 POLICY
 
   website {
+    redirect_all_requests_to = "https://${var.root_domain_name}"
+  }
+}
+
+resource "aws_s3_bucket" "ryanrishi-com" {
+  bucket = var.root_domain_name
+  acl    = "public-read"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AddPerm",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": ["arn:aws:s3:::${var.root_domain_name}/*"]
+    }
+  ]
+}
+POLICY
+
+  website {
     index_document = "index.html"
     error_document = "404.html"
   }
 }
 
 resource "aws_acm_certificate" "certificate" {
-  domain_name               = "*.${var.root_domain_name}"
+  domain_name               = var.root_domain_name
   validation_method         = "EMAIL"
   subject_alternative_names = [var.www_domain_name]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_cloudfront_distribution" "www_distribution" {
@@ -55,7 +82,6 @@ resource "aws_cloudfront_distribution" "www_distribution" {
   }
 
   enabled             = true
-  default_root_object = "index.html"
 
   default_cache_behavior {
     viewer_protocol_policy  = "redirect-to-https"
@@ -90,6 +116,54 @@ resource "aws_cloudfront_distribution" "www_distribution" {
   }
 }
 
+resource "aws_cloudfront_distribution" "ryanrishi-com" {
+  origin {
+    domain_name = aws_s3_bucket.ryanrishi-com.website_endpoint
+    origin_id   = var.root_domain_name
+
+    custom_origin_config {
+      http_port               = "80"
+      https_port              = "443"
+      origin_protocol_policy  = "http-only"
+      origin_ssl_protocols    = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+
+  default_cache_behavior {
+    viewer_protocol_policy  = "redirect-to-https"
+    compress                = true
+    allowed_methods         = ["GET", "HEAD"]
+    cached_methods          = ["GET", "HEAD"]
+    target_origin_id        = var.root_domain_name
+    min_ttl                 = 0
+    default_ttl             = 86400
+    max_ttl                 = 31536000
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  aliases = [var.root_domain_name]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
 resource "aws_route53_zone" "zone" {
   name = var.root_domain_name
 }
@@ -102,6 +176,18 @@ resource "aws_route53_record" "www" {
   alias {
     name                   = aws_cloudfront_distribution.www_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.www_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name    = var.root_domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.ryanrishi-com.domain_name
+    zone_id                = aws_cloudfront_distribution.ryanrishi-com.hosted_zone_id
     evaluate_target_health = false
   }
 }
