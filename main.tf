d6d12379cfa3fa5354048abbd9fa1bf683de96ea
Ryan Rishi <ryan@ryanrishi.com>
2020-01-13 18:53:00 -0800

    update certificate to use wildcard SAN

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ POLICY
 resource "aws_acm_certificate" "certificate" {
   domain_name               = var.root_domain_name
   validation_method         = "EMAIL"
-  subject_alternative_names = [var.www_domain_name]
+  subject_alternative_names = ["*.${var.root_domain_name}"]
 
   lifecycle {
     create_before_destroy = true
