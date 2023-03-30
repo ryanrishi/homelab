1c484b52b299b00495245582eda76f5cd270af0a
Ryan Rishi <ryan@ryanrishi.com>
2023-03-30 19:47:26 -0400

    rm dead code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,3 @@
-# resource "proxmox_vm_qemu" "cloud-init-test" {
-#   target_node = "ryanrishi"
-#   name        = "cloud-init-test"
-#   full_clone  = false
-#   memory      = 2048
-#   onboot      = true
-#   # oncreate         = false
-#   automatic_reboot = true
-#   scsihw           = "virtio-scsi-pci"
-#   clone            = local.cloud_init_template
-#   os_type          = "cloud-init"
-#   ipconfig0        = "ip=192.168.1.201/24,gw=192.168.1.1"
-#   # cicustom =
-
-#   disk {
-#     type    = "scsi"
-#     storage = "local-lvm"
-#     size    = "2G"
-#   }
-
-#   network {
-#     bridge = "vmbr0"
-#     model  = "virtio"
-#   }
-
-#   sshkeys = <<EOF
-# ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMO+4LbsTRGGW2SO0F5q7WLuOCCGW/wSbMPgIo1wO0/1 ryan@ryanrishi.com
-# EOF
-# }
-
 module "test" {
   source = "./modules/cloud_init"
   name   = "cloud-init-test"
