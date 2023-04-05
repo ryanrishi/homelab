a68b1d241e09406d2e92eb30657404157e4a03aa
Ryan Rishi <ryan@ryanrishi.com>
2023-04-04 21:20:51 -0400

    give media server some more memory

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,8 @@ module "media" {
   ip     = "192.168.1.201"
 
   disk_size = "8G"
+  memory    = 8192
+  balloon   = 2048
 
   # TODO I really don't like passing this in to every VM... is there a better way to get cloud-init configs into PVE?
   pve_host     = var.pve_host
