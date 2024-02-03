da24b80372a3dd80ddd90594a279bc0cbf03c13a
Ryan Rishi <ryan@ryanrishi.com>
2024-02-03 14:55:35 -0500

    mv things back

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,7 +64,7 @@ module "k3s-primary" {
     pull:
       url: https://github.com/ryanrishi/homelab.git
       checkout: k3s
-      playbook_name: playbooks/k3s-primary.yml
+      playbook_name: k3s-primary.yml
   EOT
 
   # TODO I really don't like passing this in to every VM... is there a better way to get cloud-init configs into PVE?
