6d51a4652feb25589d3442a057290663519f1c84
Ryan Rishi <ryan@ryanrishi.com>
2024-02-07 16:59:22 -0500

    add k3s-agent playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,14 +102,54 @@ module "k3s-primary" {
   pve_password = var.pve_password
 }
 
-module "k3s-workers" {
-  count  = 2
+module "k3s-agent" {
+  count  = 1
   source = "./modules/cloud_init"
   name   = "k3s-replica-${count.index}"
 
-  cores   = 2
-  sockets = 2
-  memory  = 2048
+  cores     = 2
+  sockets   = 2
+  memory    = 2048
+  disk_size = 20
+
+  additional_cloud_init_config = <<-EOT
+  ansible:
+    install_method: distro
+    package_name: ansible-core
+    pull:
+      url: https://github.com/ryanrishi/homelab.git
+      checkout: k3s
+      playbook_name: k3s-agent.yml
+
+  # Unfortunately `cloud_final_modules` can't be merged, only overwritten
+  # This is the list from /etc/cloud/cloud.cfg with `ansible` added
+  cloud_final_modules:
+    - package-update-upgrade-install
+    - fan
+    - landscape
+    - lxd
+    - write-files-deferred
+    - puppet
+    - chef
+    - ansible  # added by ryanrishi
+    - mcollective
+    - salt-minion
+    - reset_rmc
+    - refresh_rmc_and_interface
+    - rightscale_userdata
+    - scripts-vendor
+    - scripts-per-once
+    - scripts-per-boot
+    - scripts-per-instance
+    - scripts-user
+    - ssh-authkey-fingerprints
+    - keys-to-console
+    - install-hotplug
+    - phone-home
+    - final-message
+    - power-state-change
+  EOT
+
 
   # TODO I really don't like passing this in to every VM... is there a better way to get cloud-init configs into PVE?
   pve_host     = var.pve_host
